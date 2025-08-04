585e2b903af38451b398fbed0bafc6d5e438d813
Hugo Hsi <hugohsidev@gmail.com>
2025-08-03 21:24:20 -0400

    merge

---
+++ src/lib/components/add-transaction-amount-field.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	let value = $state(0);
+	let formattedValue = $derived((value / 100).toFixed(2));
+	let componentId = $props.id();
+	let isFocused = $state(false);
+</script>
+
+<div class="flex items-center justify-center pt-12 pb-4">
+	<label for="amount-field-{componentId}">
+		<span class={['text-7xl font-bold', { 'text-primary': isFocused }]}>${formattedValue}</span>
+		<!-- svelte-ignore a11y_autofocus -->
+		<input
+			type="text"
+			bind:value
+			onfocus={() => (isFocused = true)}
+			onblur={() => (isFocused = false)}
+			id="amount-field-{componentId}"
+			inputmode="numeric"
+			autofocus
+			class="size-0 opacity-0"
+		/>
+	</label>
+</div>
--- src/lib/components/add-transaction-drawer.svelte
+++ src/lib/components/add-transaction-drawer.svelte
@@ -3,6 +3,9 @@
 	import * as Drawer from '$lib/components/ui/drawer/index.js';
 	import { Portal } from 'bits-ui';
 
+	import AddTransactionAmountField from './add-transaction-amount-field.svelte';
+	import AddTransactionNameField from './add-transaction-name-field.svelte';
+	import AddTransactionPaidAtField from './add-transaction-paid-at-field.svelte';
 	import { Button } from './ui/button';
 </script>
 
@@ -16,14 +19,26 @@
 			{/snippet}
 		</Drawer.Trigger>
 	</Portal>
-	<Drawer.Content>
-		<Drawer.Header>
-			<Drawer.Title>Are you sure absolutely sure?</Drawer.Title>
-			<Drawer.Description>This action cannot be undone.</Drawer.Description>
-		</Drawer.Header>
-		<Drawer.Footer>
-			<Button>Submit</Button>
-			<Drawer.Close>Cancel</Drawer.Close>
-		</Drawer.Footer>
+	<Drawer.Content class="border-none bg-transparent">
+		<!-- BG Override -->
+		<div class="mt-2 rounded-t-3xl border-t bg-background">
+			<!-- New Header Container -->
+			<div class="flex items-center justify-between p-2 pt-3">
+				<Drawer.Close>
+					{#snippet child({ props })}
+						<Button variant="link" class="text-lg" {...props}>Cancel</Button>
+					{/snippet}
+				</Drawer.Close>
+				<span class="absolute left-1/2 -translate-x-1/2 transform text-lg font-bold"
+					>New Transaction</span
+				>
+				<Button variant="link" class="text-lg font-bold">Done</Button>
+			</div>
+			<div class="mb-6 flex flex-col gap-6 px-6">
+				<AddTransactionAmountField />
+				<AddTransactionNameField />
+				<AddTransactionPaidAtField />
+			</div>
+		</div>
 	</Drawer.Content>
 </Drawer.Root>
