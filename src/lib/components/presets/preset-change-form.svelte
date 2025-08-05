2b4dcc46356d2aa784f72ed3b5924c9dbe7b6e3f
RonanCraft <SuperRonanCraft@gmail.com>
2025-08-05 14:49:51 +0000

    change preset

---
+++ src/lib/components/presets/preset-change-form.svelte
@@ -0,0 +1,30 @@
+<script lang="ts" module>
+	export const changePresetSchema = z.object({
+		amount: z.string().refine((amount) => !isNaN(Number(amount))),
+		name: z.string(),
+		id: z.string(),
+	});
+</script>
+
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	import { changePreset } from '$lib/remote/preset.remote';
+	import z from 'zod';
+	let { children, drawerOpen = $bindable() }: { children: Snippet; drawerOpen: boolean } = $props();
+</script>
+
+<form
+	{...changePreset.enhance(async ({ submit }) => {
+		try {
+			// const data = Object.fromEntries(formData.entries());
+			// const validatedData = addTransactionSchema.parse(data);
+			await submit();
+			drawerOpen = false;
+		} catch {
+			console.error('something went wrong');
+		}
+	})}
+>
+	{@render children()}
+</form>
--- src/lib/remote/preset.remote.ts
+++ src/lib/remote/preset.remote.ts
@@ -1,5 +1,6 @@
 import { command, form, query } from '$app/server';
 import { addPresetSchema } from '$lib/components/presets/preset-add-form.svelte';
+import { changePresetSchema } from '$lib/components/presets/preset-change-form.svelte';
 import { ERRORS } from '$lib/server/errors';
 import { DBService } from '$lib/server/service/db';
 import z from 'zod';
@@ -23,23 +24,18 @@ export const createPreset =  form(async (formData) => {
 	await getPresets().refresh();
 });
 
-// command(
-// 	z.object({ amount: z.string(), name: z.string() }),
-// 	async ({ amount, name }) => {
-// 		const dbService = new DBService();
-// 		const newPreset = await dbService.insertPreset({ amount, name });
-// 		await getPresets().refresh();
-// 		return newPreset;
-// 	}
-// );
+export const changePreset = form(async (formData) => {
+	const rawData = Object.fromEntries(formData.entries());
+	const result = changePresetSchema.safeParse(rawData);
 
-export const changePreset = command(
-	z.object({ amount: z.string(), id: z.string(), name: z.string() }),
-	async ({ amount, id, name }) => {
-		const dbService = new DBService();
-		await dbService.updatePreset({ amount, id, name });
-		await getPresets().refresh();
+	if (!result.success) {
+		return ERRORS.INTERNAL_SERVER_ERROR;
 	}
+
+	const dbService = new DBService();
+		await dbService.updatePreset({ ...result.data });
+		await getPresets().refresh();
+	
 );
 
 export const deletePreset = command(z.object({ id: z.string() }), async ({ id }) => {
